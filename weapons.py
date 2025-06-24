b5e783298fb0553abc09bdf32da7e02ec8098722
Jayvee <bustamantejayvee6@gmail.com>
2025-06-24 21:55:33 +0800

    finished creating the weapon class and its attribute, also created three weapons

--- weapons.py
+++ weapons.py
@@ -1,2 +1,10 @@
-class Weapon:
-    
+class Weapons:
+    def __init__(self, name: str, weapon_type:str, damage: int, value: int) -> None:
+        self.name = name
+        self.weapon_Type = weapon_type
+        self.damage = damage
+        self.value = value
+        
+iron_sword = Weapons(name="Iron Sword", weapon_type="sharp", damage=5, value=10)
+short_bow = Weapons(name="Short Bow", weapon_type-"range", damage=4, value=8)
+unarmed = Weapons(name="Fist", weapon_type="blunt", damage=2, value=0)
