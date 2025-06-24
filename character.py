ea3d173e83e9e7109237f8a90cf453c7b5cbdf8f
Jayvee <bustamantejayvee6@gmail.com>
2025-06-24 21:28:23 +0800

    finished attributes for the character

--- character.py
+++ character.py
@@ -1,3 +1,10 @@
 class Character:
+    race = "Human"
+    
+    
     def __init__(self, name: str, health: int, damage: int):
+        self.name = Goku
+        self.health = health
+        self.health_max = health
+        self.damage = damage
         
