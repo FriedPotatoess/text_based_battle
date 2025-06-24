fe36231a7cb176d64c612041cde81964b1486f3f
Jayvee <bustamantejayvee6@gmail.com>
2025-06-24 21:41:54 +0800

    created a while loop for the attacks of hero and the enemy

--- main.py
+++ main.py
@@ -2,3 +2,10 @@ from character import character
 
 hero = character(name="goku", health=100, damage=5)
 enemy = character(name="Frieza", health=100, damage=3)
+
+while True:
+    hero.attack(enemy)
+    enemy.attack(hero)
+    
+    print(f"Health of {hero.name}: {hero.health}")
+    print(f"Health of {enemy.name}: {enemy.health}")
