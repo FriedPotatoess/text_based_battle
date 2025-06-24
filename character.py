65040fa68a07eb0f0ad86247bc1cd8c377f3ae40
Jayvee <bustamantejayvee6@gmail.com>
2025-06-24 22:12:43 +0800

    created another class, Hero, that inherits from the Character class

--- character.py
+++ character.py
@@ -15,5 +15,7 @@ class Character:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
         
-        
+class Hero(Character):
+    def __init__(self, name: str, health: int) -> None:
+        super().__init__(name=name, health=health)
         
