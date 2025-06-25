25216e5590741dad56a725ef18414f63d7966411
Jayvee <bustamantejayvee6@gmail.com>
2025-06-25 19:56:07 +0800

    Saved the isntance variables of the healthbar

--- health_bar.py
+++ health_bar.py
@@ -3,5 +3,12 @@ import os
 os.sytem("")
 
 class HealthBar:
-    def __init__(self, entity, length: int, is_colored: bool,color: str) -> None:
+    def __init__(self, entity, length: int = 20, is_colored: bool = True,color: str = "") -> None:
+        self.entity = entity
+        self.lenght = lenght
+        self.max_value = entity.health_max
+        self.current_value = entity.health
         
+        
+        self.is_colored = is_colored
+        self.color = color
