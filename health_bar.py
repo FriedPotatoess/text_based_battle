367bc27eb142e43b867410446a3fa3edd24cc8af
Jayvee <bustamantejayvee6@gmail.com>
2025-06-25 20:01:04 +0800

    Other properties implemented like the symbols and the decorating barrier, defined on a class level. Also added a dictionaries for color values, and will be accessed by the color paramater

--- health_bar.py
+++ health_bar.py
@@ -3,6 +3,21 @@ import os
 os.sytem("")
 
 class HealthBar:
+    symbol_remaining: str = "█"
+    barrier: str = "|"
+    colors: dict = {"red": "\033[91m",
+                    "purple": "\33[95m",
+                    "blue": "\33[34m",
+                    "blue2": "\33[36m",
+                    "blue3": "\33[96m",
+                    "green": "\033[92m",
+                    "green2": "\033[32m",
+                    "brown": "\33[33m",
+                    "yellow": "\33[93m",
+                    "grey": "\33[37m",
+                    "default": "\033[0m"
+                    }
+    
     def __init__(self, entity, length: int = 20, is_colored: bool = True,color: str = "") -> None:
         self.entity = entity
         self.lenght = lenght
